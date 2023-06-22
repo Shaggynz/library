e17f9996862c65488597d98e7a85bc9c54a8137a
Shaggynz <137244604+Shaggynz@users.noreply.github.com>
2023-06-22 14:17:11 +1200

    Update config.php

--- includes/config.php
+++ includes/config.php
@@ -1,13 +1,13 @@
 <?php 
 // DB credentials.
-define('AZURE_MYSQL_HOST','aislibrarywebpage-server.mysql.database.azure.com');
-define('AZURE_MYSQL_USERNAME','daqzrclbpt');
-define('AZURE_MYSQL_PASSWORD','S5SZTZS6TU78ZC0T$');
+define('DB_HOST','localhost');
+define('DB_USER','root');
+define('DB_PASS','');
 define('DB_NAME','library');
 // Establish database connection.
 try
 {
-$dbh = new PDO("mysql:host=".AZURE_MYSQL_HOST.";dbname=".DB_NAME,AZURE_MYSQL_USERNAME, AZURE_MYSQL_PASSWORD,array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES 'utf8'"));
+$dbh = new PDO("mysql:host=".DB_HOST.";dbname=".DB_NAME,DB_USER, DB_PASS,array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES 'utf8'"));
 }
 catch (PDOException $e)
 {
